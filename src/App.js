b464ba5be28b654776df246a3c64d9918f79a645
Alex Vaschilov <vaschilovalex@gmail.com>
2023-05-07 02:59:17 +0300

    Add Course component, refactor the app so that it work regardless of the number of parts a course has

--- src/App.js
+++ src/App.js
@@ -1,78 +1,81 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.name}</h1>
-    </>
-  );
-}
+// const Total = (props) => {
+//   return (
+//     <>
+//       <p>
+//         Number of exercises {
+//           props.parts[0].exercises + 
+//           props.parts[1].exercises + 
+//           props.parts[2].exercises
+//         }.
+//       </p>
+//     </>
+//   );
+// }
 
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0]} />
-
-      <Part part={props.parts[1]} />
-
-      <Part part={props.parts[2]} />
-    </>
-  );
-}
-
-const Part = (props) => {
+const Part = ({ part }) => {
   return (
     <>
       <p>
-        {props.part.name} {props.part.exercises}
+        {part.name} {part.exercises}
       </p>
     </>
   )
 }
 
-const Total = (props) => {
+const Header = ({ name }) => {
   return (
     <>
-      <p>
-        Number of exercises {
-          props.parts[0].exercises + 
-          props.parts[1].exercises + 
-          props.parts[2].exercises
-        }.
-      </p>
+      <h1>{name}</h1>
     </>
   );
 }
 
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map(part => 
+        <Part key={part.id} part={part} />  
+      )}
+    </>
+  );
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
 
     parts: [
       {
         name: "Fundamentals of React",
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
 
       {
         name: "Using props to pass data",
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
 
       {
         name: "State of a component",
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header name={course.name} />
-
-      <Content parts={course.parts} />
-
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 }
 
 export default App;
